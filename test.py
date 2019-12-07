209634b12e213eddcf7ea5e30cf1cd1718fc8701
serkan Gecici <geciciserkan@gmail.com>
2019-12-07 15:15:29 +0000

    refactoring
    refactoring

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
 import unittest
-from PythonGoogleBookSearchApp import GoogleBooks
+from PythonGoogleBookSearchApp import *
 
 class TestSum(unittest.TestCase):
     googleBook=GoogleBooks()
     def testSearch(self):
         self.assertEqual(len(self.googleBook.search("London"))>0 , True, "Search Result should be available for London keyword")
+        self.assertEqual(self.googleBook.printBooksToScreen(PrintType.SEARCH_RESULT) is not None , True, "Program should return  search result")
         self.assertEqual(len(self.googleBook.saveBook(1))>0 , True, "Reading list must be avaiable after saving a book to reading list")
-        self.assertEqual(self.googleBook.printBooksToScreen(1) is not None , True, "Program should return  search result")
-        self.assertEqual(self.googleBook.printBooksToScreen(2) is not None , True, "Program should return reading list")
-        self.assertEqual(len(self.googleBook.selectCommand()) is not None , True, "Program should read command from console")
-        self.assertEqual(len(self.googleBook.readCommand(0)) is not None , True, "Program should read command from console")
+        self.assertEqual(self.googleBook.printBooksToScreen(PrintType.READING_LIST) is not None , True, "Program should return reading list")
 
 if __name__ == '__main__':
     unittest.main()
